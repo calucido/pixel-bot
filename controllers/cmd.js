9e5709cc019c02f02b3612e07bd9e84893a5a04e
Calvin Lucido <clucido@intermarkets.net>
2019-06-23 17:15:01 -0400

    inconsistent /help command

--- controllers/cmd.js
+++ controllers/cmd.js
@@ -192,7 +192,7 @@ module.exports = app => {
             });
           }).catch(handleError);
         }
-      } else if (message.text.match(/^\/timezone /i)) {
+      } else if (message.text.match(/^\/timezone/i)) {
         let timezone = message.text.replace(/^\/timezone +/i, '');
         if (!moment.tz.zone(timezone)) {
          return send(message.chat.it, "I don't recognize that timezone. Make sure to use the boring, technical name, like \"US/Eastern\".", handleError);
@@ -203,7 +203,7 @@ module.exports = app => {
           return send(message.chat.id, `Set your timezone to ${user.timezone}.`, handleError);
         });
       } else if (message.text.match(/^\/help/i)) { // send list of commands
-        return send(message.chat.id, `/am - Set the am mood.\n/pm - Set the pm mood.\n/color - Define a new color (usage: /color name #hex "mood")\n/year - See a graph of the year (usage: /year #### am)\n/colors - See a list of your defined colors.\n/tz - Set your timezone.`, handleError);
+        return send(message.chat.id, `/am - Set the am mood.\n/pm - Set the pm mood.\n/color - Define a new color (usage: /color name #hex "mood")\n/year - See a graph of the year (usage: /year #### am)\n/colors - See a list of your defined colors.\n/timezone - Set your timezone.`, handleError);
       } else { // I have no idea what you're saying
         return send(message.chat.id, "What does that mean? Say /help to see what I can do.", handleError);
       }
